96a2e8085f0b86e7f5292ffffcae2a00088522dc
Diego Acuña <45692265+diegoaces@users.noreply.github.com>
2023-12-27 09:41:47 -0300

    Revert "Form vuelo"

--- coreplugins/form-vuelo/templates/load_buttons.js
+++
@@ -1,5 +0,0 @@
-PluginsAPI.Dashboard.addTaskActionButton(['form-vuelo/build/Form.js'], function (args, Form) {
-        var task = args.task;
-        return React.createElement(Form, {taskId: task.id, apiURL: "{{ api_url }}",});
-    }
-);
